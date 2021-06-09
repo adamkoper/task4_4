8911c160ff5fb0fb941663ddcd172b2af2328550
Adam Koprowski <adamkoprowski4@gmail.com>
2021-06-09 19:24:15 +0200

    fixed info "I think"

--- task4_4.py
+++ task4_4.py
@@ -1,26 +1,27 @@
-import sys
+
 import logging
+logging.basicConfig(level=logging.INFO)
 
 def calculator(operation):
     if operation == 1:
-        logging.info("Dodaję ", num1, "i ", num2)
+        logging.info(f"Dodaję {num1} i {num2}")
         result_1 = num1+num2
         print("Wynik to ", result_1)
     elif operation == 2:
-        logging.info("Odejmuję ", num1, "i ", num2)
+        logging.info(f"Odejmuję {num1} i {num2}")
         result_2 = num1-num2
         print("Wynik to ", result_2)
     elif operation == 3:
-        logging.info("Mnożę ", num1, "i ", num2)
+        logging.info(f"Mnożę {num1} i {num2}")
         result_3 = num1*num2
         print("Wynik to ", result_3)
     elif operation == 4:
-        logging.info("Dzielę", num1, "i ", num2)
+        logging.info(f"Dzielę {num1} i {num2}")
         result_4 = num1/num2
         print("Wynik to ", result_4)
     else:
         print("podana wartość nie jest liczbą od 1 do 4 ")
-    pass
+
 
 if __name__ == "__main__":
     operation = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))

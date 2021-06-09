a250c7f8d80fbdc75664c1b819994cad918988c2
Adam Koprowski <adamkoprowski4@gmail.com>
2021-06-09 19:14:58 +0200

    change return to print

--- task4_4.py
+++ task4_4.py
@@ -3,28 +3,29 @@ import logging
 
 def calculator(operation):
     if operation == 1:
-        return logging.info("Dodaję ", num1, "i ", num2)
+        logging.info("Dodaję ", num1, "i ", num2)
         result_1 = num1+num2
-        return "Wynik to ", result_1
+        print("Wynik to ", result_1)
     elif operation == 2:
-        return logging.info("Odejmuję ", num1, "i ", num2)
+        logging.info("Odejmuję ", num1, "i ", num2)
         result_2 = num1-num2
-        return "Wynik to ", result_2
+        print("Wynik to ", result_2)
     elif operation == 3:
-        return logging.info("Mnożę ", num1, "i ", num2)
+        logging.info("Mnożę ", num1, "i ", num2)
         result_3 = num1*num2
-        return "Wynik to ", result_3
+        print("Wynik to ", result_3)
     elif operation == 4:
-        return logging.info("Dzielę", num1, "i ", num2)
+        logging.info("Dzielę", num1, "i ", num2)
         result_4 = num1/num2
-        return "Wynik to ", result_4
+        print("Wynik to ", result_4)
     else:
-        return "podana wartość nie jest liczbą od 1 do 4 "
+        print("podana wartość nie jest liczbą od 1 do 4 ")
+    pass
 
 if __name__ == "__main__":
-    operation = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
-    num1 = input("Podaj składnik 1. ")
-    num2 = input("Podaj składnik 2. ")
+    operation = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
+    num1 = int(input("Podaj składnik 1. "))
+    num2 = int(input("Podaj składnik 2. "))
     print(calculator(operation))
 
 
